db2563e3db8850c8f6db18a4165b0ab6ddafd761
Jonasz Bigda <jonasz@bankai.pl>
2020-12-06 15:00:22 +0100

    remove "DEBUG" flag

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class App extends StatelessWidget {
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
           home: HomePage(),
+          debugShowCheckedModeBanner: false,
         ));
   }
 }
